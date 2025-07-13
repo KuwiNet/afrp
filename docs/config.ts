0c091f1e5a54a7423cab7beb674964b796ed788b
KuWi <darnycn@gmail.com>
2025-07-14 04:45:42 +0800

    Update config.ts

--- docs/config.ts
+++ docs/config.ts
@@ -14,8 +14,8 @@ export default defineAdditionalConfig({
     search: { options: searchOptions() },
 
     sidebar: {
-      '/zh/guide/': { base: '/zh/guide/', items: sidebarGuide() },
-      '/zh/reference/': { base: '/zh/reference/', items: sidebarReference() }
+      '/guide/': { base: '/guide/', items: sidebarGuide() },
+      '/reference/': { base: '/reference/', items: sidebarReference() }
     },
 
     editLink: {
@@ -63,13 +63,13 @@ function nav(): DefaultTheme.NavItem[] {
   return [
     {
       text: '指南',
-      link: '/zh/guide/what-is-vitepress',
-      activeMatch: '/zh/guide/'
+      link: '/guide/what-is-vitepress',
+      activeMatch: '/guide/'
     },
     {
       text: '参考',
-      link: '/zh/reference/site-config',
-      activeMatch: '/zh/reference/'
+      link: '/reference/site-config',
+      activeMatch: '/reference/'
     },
     {
       text: pkg.version,
@@ -129,7 +129,7 @@ function sidebarGuide(): DefaultTheme.SidebarItem[] {
         { text: 'sitemap 生成', link: 'sitemap-generation' }
       ]
     },
-    { text: '配置和 API 参考', base: '/zh/reference/', link: 'site-config' }
+    { text: '配置和 API 参考', base: '/reference/', link: 'site-config' }
   ]
 }
 
@@ -144,7 +144,7 @@ function sidebarReference(): DefaultTheme.SidebarItem[] {
         { text: 'CLI', link: 'cli' },
         {
           text: '默认主题',
-          base: '/zh/reference/default-theme-',
+          base: '/reference/default-theme-',
           items: [
             { text: '概览', link: 'config' },
             { text: '导航栏', link: 'nav' },
